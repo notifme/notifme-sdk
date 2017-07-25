52c19dd90ada22552b4a5633b757ef6750c1ff57
David Brown <david@devanco.com>
2017-07-25 18:17:37 +0200

    first example

---
+++ examples/development.js
@@ -0,0 +1,29 @@
+/* @flow */
+import NotifmeSdk from '../src'
+// Types
+import type {NotificationRequestType} from '../src'
+
+const notifmeSdk = new NotifmeSdk({})
+
+const notificationRequest: NotificationRequestType = {
+  email: {
+    from: 'me@example.com',
+    to: 'you@example.com',
+    subject: 'Hi',
+    text: '...'
+  },
+  push: {
+    title: 'Hi',
+    body: '...'
+  },
+  sms: {
+    from: 'Notif.me',
+    to: '+10000000000',
+    text: '...'
+  },
+  webpush: {
+    title: 'Hi',
+    body: '...'
+  }
+}
+notifmeSdk.send(notificationRequest)
