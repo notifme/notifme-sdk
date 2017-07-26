5ee53807d361631c2c959fa5d38af76860b96318
David Brown <david@devanco.com>
2017-07-26 18:59:09 +0200

    SMTP provider

--- src/providers/email/smtp.js
+++ src/providers/email/smtp.js
@@ -1,16 +1,19 @@
 /* @flow */
+import nodemailer from 'nodemailer'
 // Types
 import type {EmailRequestType} from '../../model-request'
 
 export default class EmailSmtpProvider {
   name: string
+  transporter: Object
 
   constructor (config: Object) {
     this.name = 'email-smtp-provider'
+    this.transporter = nodemailer.createTransport(config)
   }
 
   async send (request: EmailRequestType): Promise<string> {
-    // TODO: implement SMTP
-    return ''
+    const result = await this.transporter.sendMail(request)
+    return result.messageId
   }
 }
