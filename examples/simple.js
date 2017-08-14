ff26239c7046b8e4a19e8ff55a840fe518d0caf0
David Brown <david@devanco.com>
2017-08-14 14:32:16 +0200

    doc add simple non-babel example

---
+++ examples/simple.js
@@ -0,0 +1,38 @@
+/* @flow */
+const NotifmeSdk = require('../src').default // notifme-sdk
+
+const notifmeSdk = new NotifmeSdk({})
+
+const notificationRequest = {
+  email: {
+    from: 'me@example.com',
+    to: 'john@example.com',
+    subject: 'Hi John',
+    html: '<b>Hello John! How are you?</b>'
+  },
+  sms: {
+    from: '+15000000000',
+    to: '+15000000001',
+    text: 'Hello John! How are you?'
+  },
+  push: {
+    registrationToken: 'xxxxx',
+    title: 'Hi John',
+    body: 'Hello John! How are you?',
+    icon: 'https://notifme.github.io/notifme-sdk/img/icon.png'
+  },
+  webpush: {
+    subscription: {
+      keys: {
+        auth: 'xxxxx',
+        p256dh: 'xxxxx'
+      },
+      endpoint: 'xxxxx'
+    },
+    title: 'Hi John',
+    body: 'Hello John! How are you?',
+    icon: 'https://notifme.github.io/notifme-sdk/img/icon.png'
+  }
+}
+
+notifmeSdk.send(notificationRequest).then(console.log)
