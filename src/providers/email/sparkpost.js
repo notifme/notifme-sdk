d669921923ee839e8f7cb78b9f071ca31077ce33
David Brown <david@devanco.com>
2017-08-18 14:30:06 +0200

    call sparkpost api with node-fetch #3 #11

--- src/providers/email/sparkpost.js
+++ src/providers/email/sparkpost.js
@@ -1,40 +1,59 @@
 /* @flow */
-import SparkPost from 'sparkpost'
+import fetch from 'node-fetch'
 // types
 import type {EmailRequestType} from '../../models/notification-request'
 
 export default class EmailSparkPostProvider {
   id: string
-  client: Object
+  apiKey: string
 
   constructor (config: Object) {
     this.id = 'email-sparkpost-provider'
-    this.client = new SparkPost(config.apiKey, {
-      stackIdentity: 'notifme'
-    })
+    this.apiKey = config.apiKey
   }
 
-  send (request: EmailRequestType): Promise<string> {
-    let txBody = {
-      options: {
-        transactional: true
+  async send (request: EmailRequestType): Promise<string> {
+    const {id, userId, from, replyTo, subject, html, text, headers, to, cc, bcc, attachments} = request
+    const response = await fetch('https://api.sparkpost.com/api/v1/transmissions', {
+      method: 'POST',
+      headers: {
+        Authorization: this.apiKey,
+        'Content-Type': 'application/json'
       },
-      content: {
-        from: request.from,
-        reply_to: request.replyTo,
-        subject: request.subject,
-        html: request.html,
-        text: request.text,
-        headers: request.headers
-      },
-      recipients: [
-        {address: request.to}
-      ],
-      cc: request.cc,
-      bcc: request.bcc
-      // TODO: attachments
+      body: JSON.stringify({
+        options: {
+          transactional: true
+        },
+        content: {
+          from,
+          reply_to: replyTo,
+          subject,
+          html,
+          text,
+          headers,
+          attachments: (attachments || []).map(({contentType, filename, content}) =>
+            ({
+              type: contentType,
+              name: filename,
+              data: (typeof content === 'string' ? Buffer.from(content) : content).toString('base64')
+            }))
+        },
+        recipients: [
+          {address: to}
+        ],
+        cc,
+        bcc,
+        metadata: {id, userId}
+      })
+    })
+
+    const responseBody = await response.json()
+    if (response.ok) {
+      return responseBody.results.id
+    } else {
+      const [firstError] = responseBody.errors
+      const message = Object.keys(firstError).map((key) => `${key}: ${firstError[key]}`).join(', ')
+      throw new Error(`${response.status} - ${message}`)
     }
-    return this.client.transmissions.send(txBody)
-      .then((data) => data.results.id)
   }
 }
