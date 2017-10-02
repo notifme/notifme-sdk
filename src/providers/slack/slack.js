0f028c1d52615a99d1f1e0bf738309096ab54fb9
TheConnMan <bcconn2112@gmail.com>
2017-10-02 19:35:41 -0400

    Fall back to vanilla REST for Slack

--- src/providers/slack/slack.js
+++ src/providers/slack/slack.js
@@ -1,21 +1,32 @@
 /* @flow */
-import SlackWebhook from 'slack-webhook'
+import fetch from 'node-fetch'
 // Types
 import type {SlackRequestType} from '../../models/notification-request'
 
 export default class SlackProvider {
   id: string = 'slack-provider'
-  slack: SlackWebhook
+  webhookUrl: string
 
   constructor (config: Object) {
-    this.slack = new SlackWebhook(config.webhookUrl)
+    this.webhookUrl = config.webhookUrl
   }
 
   async send (request: SlackRequestType): Promise<string> {
-    try {
-      return await this.slack.send(request.text)
-    } catch (e) {
-      throw new Error(e)
+    const body = JSON.stringify({
+      text: request.text
+    })
+    const apiRequest = {
+      method: 'POST',
+      body
+    }
+
+    const response = await fetch(this.webhookUrl, apiRequest)
+
+    const responseText = await response.text()
+    if (response.ok) {
+      return responseText
+    } else {
+      throw new Error(`${response.status} - ${responseText}`)
     }
   }
 }
