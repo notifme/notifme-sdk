5702ba0177f5246248a3460fd8c1430c9224afb3
David Brown <david@devanco.com>
2017-10-04 14:25:27 +0200

    upgrade notification-catcher (with slack)

--- src/providers/slack/notificationCatcher.js
+++ src/providers/slack/notificationCatcher.js
@@ -5,11 +5,16 @@ import type {SlackRequestType} from '../../models/notification-request'
 
 export default class SlackCatcherProvider extends NotificationCatcherProvider {
   async send ({text}: SlackRequestType): Promise<string> {
-    return this.sendToCatcher({
-      to: 'webhook@slack',
+    this.sendToCatcher({
+      to: 'public.channel@slack',
       from: '-',
-      subject: 'Webhook',
-      text
+      subject: `${text.substring(0, 20)}${text.length > 20 ? '...' : ''}`,
+      text,
+      headers: {
+        'X-type': 'slack',
+        'X-to': '[slack public channel]'
+      }
     })
+    return ''
   }
 }
