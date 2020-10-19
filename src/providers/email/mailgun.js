d508485864ed1b737051779ba9bd5a09cc98d46f
Badji Lounes <lounes.badji@cpe.fr>
2020-10-19 17:15:06 +0200

    - fix: mailgun apiBasePath configuration added

--- src/providers/email/mailgun.js
+++ src/providers/email/mailgun.js
@@ -8,10 +8,12 @@ export default class EmailMailgunProvider {
   id: string = 'email-mailgun-provider'
   apiKeyBase64: string
   domainName: string
+  apiBasePath: string
 
   constructor (config: Object) {
     this.apiKeyBase64 = Buffer.from(`api:${config.apiKey}`).toString('base64')
     this.domainName = config.domainName
+    this.apiBasePath = config?.apiBasePath ?? 'https://api.mailgun.net/v3';
   }
 
   async send (request: EmailRequestType): Promise<string> {
@@ -35,7 +37,7 @@ export default class EmailMailgunProvider {
     if (id) form.append('v:Notification-Id', id)
     if (userId) form.append('v:User-Id', userId)
 
-    const response = await fetch(`https://api.mailgun.net/v3/${this.domainName}/messages`, {
+    const response = await fetch(`${this.apiBasePath}/${this.domainName}/messages`, {
       method: 'POST',
       headers: {
         Authorization: `Basic ${this.apiKeyBase64}`,
