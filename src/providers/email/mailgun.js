da0d575a01a5303afa28c41e5026ed4e7c1fae38
Badji Lounes <lounes.badji@cpe.fr>
2020-10-19 17:35:35 +0200

    - clean: host naming - api version configuration added

--- src/providers/email/mailgun.js
+++ src/providers/email/mailgun.js
@@ -8,12 +8,14 @@ export default class EmailMailgunProvider {
   id: string = 'email-mailgun-provider'
   apiKeyBase64: string
   domainName: string
-  apiBasePath: string
+  host: string
+  version: string
 
   constructor (config: Object) {
     this.apiKeyBase64 = Buffer.from(`api:${config.apiKey}`).toString('base64')
     this.domainName = config.domainName
-    this.apiBasePath = config.apiBasePath || 'https://api.mailgun.net/v3';
+    this.host = config.host || 'api.mailgun.net';
+    this.version = config.version || 'v3';
   }
 
   async send (request: EmailRequestType): Promise<string> {
@@ -37,7 +39,7 @@ export default class EmailMailgunProvider {
     if (id) form.append('v:Notification-Id', id)
     if (userId) form.append('v:User-Id', userId)
 
-    const response = await fetch(`${this.apiBasePath}/${this.domainName}/messages`, {
+    const response = await fetch(`https://${this.host}/${this.version}/${this.domainName}/messages`, {
       method: 'POST',
       headers: {
         Authorization: `Basic ${this.apiKeyBase64}`,
