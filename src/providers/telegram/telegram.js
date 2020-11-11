5599723b5192224cbdb57ea846ce73c019ddf6a0
Duy Đat Phạm <duydatpham@gmail.com>
2020-11-11 13:22:25 +0700

    #update config telegram

--- src/providers/telegram/telegram.js
+++ src/providers/telegram/telegram.js
@@ -7,10 +7,10 @@ import type { TelegramRequestType } from '../../models/notification-request'
 export default class TelegramProvider {
   id: string = 'telegram-provider'
   bot_token: string
-  base_url: string
+  base_url?: string
 
   constructor(config: Object) {
-    this.base_url = config.base_url
+    this.base_url = config.base_url || 'https://api.telegram.org'
     this.bot_token = config.bot_token
   }
 
